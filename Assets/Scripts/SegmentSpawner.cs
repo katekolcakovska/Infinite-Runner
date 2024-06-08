06d75b33b0f28b81fc3e4741c3846d87fb963681
Katerina Kolchakovska <katerina.kolchakovska@gmail.com>
2024-06-08 12:46:52 +0200

    wip

---
+++ Assets/Scripts/SegmentSpawner.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SegmentSpawner : MonoBehaviour
+{
+    public GameObject segmentPrefab;
+    public float spawnInterval;
+        public Transform spawnOrigin;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
